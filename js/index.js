68b988e9d21419a4cdacc8af421deb9df8882b84
Aleksandr Gerasimenko <38112655+AlexandrGerasimenko@users.noreply.github.com>
2023-10-14 13:32:11 +0300

    Refactor and remove btns

--- js/index.js
+++ js/index.js
@@ -2,31 +2,7 @@ let prevFileContents = null;
 let currentFileContents = null;
 let resultFileContents = null;
 
-// function onFileChange(event, callback) {
-//     console.log(event);
-//     const fileInput = event.target;
-//     const file = fileInput.files[0];
-//
-//     if (file) {
-//         const reader = new FileReader();
-//         reader.onload = function (event) {
-//             callback(event.target.result);
-//         };
-//         reader.readAsText(file);
-//     }
-// }
-//
-// function onPrevFileChange(event) {
-//     onFileChange(event, function (content) {
-//         prevFileContents = content;
-//     });
-// }
-//
-// function onCurrentFileChange(event) {
-//     onFileChange(event, function (content) {
-//         currentFileContents = content;
-//     });
-// }
+const compareBtn = document.getElementById('openInNewTab');
 
 // Функция для обработки события изменения файла
 function onPrevFileChange(event) {
@@ -72,7 +48,7 @@ function extractH1Date(content) {
     const regex = /<h1>Date:.*?<\/h1>/gs;
     let match;
     if ((match = regex.exec(content)) !== null) {
-        return  match[0];
+        return match[0];
     }
 }
 
@@ -83,9 +59,6 @@ function extractRows(content) {
     while ((match = regex.exec(content)) !== null) {
         rows.push(match[0]);
     }
-    // console.log(content);
-    // console.log(rows);
-
     return rows
 }
 
@@ -95,40 +68,16 @@ function escapeRegExp(str) {
 
 
 function extractH1PrevTagWithData(data, content) {
-    // const dateRegex = /<h1>Date: (\d{2}.\d{2}.\d{4})/;
-    // const hotelNameRegex = /Hotel: <a.*?>(.*?)<\/a>/;
-
-    // const dateMatch = data.match(dateRegex);
-    // const hotelNameMatch = data.match(hotelNameRegex);
-
-    // const date = dateMatch && dateMatch[1];
-    // const hotelName = hotelNameMatch && hotelNameMatch[1].replace(/\*/g, '\\*');;
-    // const regexString = `<h1>Date: ${date}<br>.*?${hotelName}<\/a>.*?>Back`;
-    // const regex = new RegExp(regexString, 's');
-    // const match = regex.exec(content);
-
-
     const escapedSearchString = escapeRegExp(data);
-// Создаем регулярное выражение
-//     const regexPattern = new RegExp(`${escapedSearchString}(.*?)<a class="btn btn-primary" href="#top">Back</a>`);
-
     const regexPattern = new RegExp(`${escapedSearchString}(.*?)<a class="btn btn-primary" href="#top">Back</a>`, 's');
 // Ищем соответствия
     const match = content.match(regexPattern);
-
-
-    // console.log(date, hotelName);
-    // console.log(match?.[0]);
-
-
     if (match && match[0]) {
 
         return match[0];
     }
     return null;
 }
-
-
 
 // Функция для удаления элементов массива из текста
 function removeElementsFromArray(text, elementsToRemove) {
@@ -138,58 +87,61 @@ function removeElementsFromArray(text, elementsToRemove) {
     return text;
 }
 
-// Удаляем идентичные теги <h1> из currentFileContents
-
-
-
-// Функция для открытия содержимого файла в новой вкладке
 function openFileInNewTab() {
-    // some magic for compare files
-    // Извлекаем теги <h1> из текущего и предыдущего файла
+    compareBtn.disabled = true;
     const currentH1Tags = extractH1TagsWithData(currentFileContents);
     const prevH1Tags = extractH1TagsWithData(prevFileContents);
     const equalH1 = currentH1Tags.filter((h) => prevH1Tags.includes(h));
     let preResultFileContents = removeElementsFromArray(currentFileContents, equalH1);
     const preResultH1Tags = extractH1TagsWithData(preResultFileContents);
-    preResultH1Tags.forEach((tag, index) => {
-        const preResultH1TagDate = extractH1Date(tag);
-        const prevH1TagData = extractH1PrevTagWithData(preResultH1TagDate, prevFileContents);
-        const currentH1TagData = extractH1PrevTagWithData(preResultH1TagDate, currentFileContents);
-        if(!prevH1TagData) return;
-        // console.log(prevH1TagData);
+    preResultH1Tags.forEach((tag) => {
+            const preResultH1TagDate = extractH1Date(tag);
+            const prevH1TagData = extractH1PrevTagWithData(preResultH1TagDate, prevFileContents);
+            const currentH1TagData = extractH1PrevTagWithData(preResultH1TagDate, currentFileContents);
+            if (!prevH1TagData) return;
 
-        if(prevH1TagData.includes('Nothing found')) {
-            // console.log('++++++++++Nothing found at PREV++++++++++++');
-            return;
-        }
+            if (prevH1TagData.includes('Nothing found')) {
+                return;
+            }
 
-        if(currentH1TagData.includes('Nothing found')) {
-            // console.log('/////////////////Nothing found at CURRENT///////////////');
-            return;
+            if (currentH1TagData.includes('Nothing found')) {
+                return;
+            }
+            if (tag.includes('<tbody>')) {
+                const rows = extractRows(tag);
+                rows.forEach((row) => {
+                    if (prevFileContents.includes(row)) {
+                        preResultFileContents = preResultFileContents.replace(row, '');
+                    }
+                })
+            }
         }
-        // console.log(prevH1TagData);
-        // console.log('--------------------------------------');
-        // console.log(currentH1TagData);
-        if(tag.includes('<tbody>')){
-         const rows = extractRows(tag);
-         rows.forEach((row) => {
-             if(prevFileContents.includes(row)) {
-                 // console.log(row);
-                 preResultFileContents = preResultFileContents.replace(row, '');
-                 // console.log(preResultFileContents);
-             };
-         })
-        }
-    }
     )
 
     const preResultFileContentsWithEmpty = extractH1TagsWithData(preResultFileContents);
 
     preResultFileContentsWithEmpty.forEach(t => {
-        if(t.includes('<th>') && !t.includes('<td>')
-    ){
+        if (t.includes('<th>') && !t.includes('<td>')
+        ) {
             preResultFileContents = preResultFileContents.replace(t, '');
-        }});
+        }
+    });
+
+
+    const h1WithoutTable = extractH1TagsWithData(preResultFileContents).filter(i => !i.includes('<table'));
+
+    h1WithoutTable.forEach(h1 => preResultFileContents = preResultFileContents.replace(h1, ''));
+
+    const regex = /<div class="alert alert-primary"[\s\S]*?<\/div>/g;
+    const regexForOl = /<ol[\s\S]*?<\/ol>/g;
+
+    preResultFileContents = preResultFileContents.replaceAll('<div class="alert alert-danger" role="alert">Costs not found for hotel!</div>', '')
+        .replaceAll('<div class="alert alert-danger" role="alert">Price links not found for hotel!<br>\n' +
+            'May be hotel stoped sales.</div>', '')
+        .replaceAll('<a class="btn btn-primary" href="#top">Back</a>', '')
+        .replace(regex, '')
+        .replace('<h3>Hotels list:</h3>', '')
+        .replace(regexForOl, '');
 
     resultFileContents = preResultFileContents;
 
@@ -199,11 +151,10 @@ function openFileInNewTab() {
         newTab.document.write(resultFileContents);
         newTab.document.close();
     }
+    compareBtn.disabled = false;
 }
-
-
 
 // Добавление обработчиков событий к элементам
 document.getElementById('prev-input').addEventListener('change', onPrevFileChange);
 document.getElementById('current-input').addEventListener('change', onCurrentFileChange);
-document.getElementById('openInNewTab').addEventListener('click', openFileInNewTab);
+compareBtn.addEventListener('click', openFileInNewTab);
